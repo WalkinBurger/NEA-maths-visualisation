83cbc7ebbe8b9b65a4d1dcdf2f52256237d51786
WalkinBurger <nch720@proton.me>
2025-12-21 00:51:09 +0000

    added flask

---
+++ website/views.py
@@ -0,0 +1,35 @@
+from flask import Blueprint, render_template, request, flash
+import re
+
+views = Blueprint("views", __name__)
+
+@views.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@views.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+        usertype = request.form.get("usertype")
+
+        specialChar = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+        if len(username) > 20:
+            flash("Username must be within than 20 characters.")
+        elif len(password1) > 30 or len(password1) < 8:
+            flash("Password must be within 8 to 30 characters.")
+        elif not any(ch.isdigit() for ch in password1):
+            flash("Password must contain at least one number.")
+        elif not any(ch.islower() for ch in password1):
+            flash("Password must contain at least one lowercase letter.")
+        elif not any(ch.isupper() for ch in password1):
+            flash("Password must contain at least one uppercase letter.")
+        elif not any(ch in specialChar for ch in password1):
+            flash("Password must contain at least one special character.")
+        elif password1 != password2:
+            flash("Passwords do not match.")
+        else:
+            pass
+    return render_template("signup.html")
