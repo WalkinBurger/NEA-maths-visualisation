c3c8a90d91b6f1ae621783fb1feeb22d45cbcc67
WalkinBurger <nch720@proton.me>
2026-01-03 13:41:39 +0000

    questions done kinda :3

--- website/static/questions.js
+++ website/static/questions.js
@@ -3,8 +3,8 @@ let color = (colorVar) => window.getComputedStyle(document.documentElement).getP
 let bounding = (x,y) => dg.rectangle(x,y).strokewidth(1)
 
 export function hypotenuse1() {
-    let tri = dg.polygon([dg.V2(0,0), dg.V2(0,1.3), dg.V2(2,0)])
-    let ang = dg.square(0.2).position(dg.V2(0.1,0.1))
+    let tri = dg.polygon([dg.V2(0,0), dg.V2(0,1.3), dg.V2(2,0)]).stroke(color("hl4"))
+    let ang = dg.square(0.2).position(dg.V2(0.1,0.1)).stroke(color("hl3"))
     let x = dg.textvar("x").position(dg.V2(-1.2,0.6))
     let y = dg.textvar("y").position(dg.V2(-0.2,0.9))
     let z = dg.textvar("z").position(dg.V2(-0.5,-0.4))
