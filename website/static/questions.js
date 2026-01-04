0a45f08500c6e3a1aaf5f5893d33b4db9fe41525
WalkinBurger <nch720@proton.me>
2026-01-04 16:06:52 +0000

    things

--- website/static/questions.js
+++ website/static/questions.js
@@ -1,13 +1,20 @@
 import * as dg from 'https://cdn.jsdelivr.net/npm/diagramatics@1.5/dist/diagramatics.min.js'
 let color = (colorVar) => window.getComputedStyle(document.documentElement).getPropertyValue('--'+colorVar)
-let bounding = (x,y) => dg.rectangle(x,y).strokewidth(1)
+let bounding = (x,y) => dg.rectangle(x,y).strokewidth(0)
 
-export function hypotenuse1() {
-    let tri = dg.polygon([dg.V2(0,0), dg.V2(0,1.3), dg.V2(2,0)]).stroke(color("hl4"))
+function hypotenuse (sides, posX, posY, posZ, rot, bound=null) {
+    let tri = dg.polygon([dg.V2(0,0), dg.V2(0,sides[0]), dg.V2(sides[1],0)]).stroke(color("hl4"))
     let ang = dg.square(0.2).position(dg.V2(0.1,0.1)).stroke(color("hl3"))
-    let x = dg.textvar("x").position(dg.V2(-1.2,0.6))
-    let y = dg.textvar("y").position(dg.V2(-0.2,0.9))
-    let z = dg.textvar("z").position(dg.V2(-0.5,-0.4))
-    let tri2 = tri.combine(ang).rotate(2)
+    let x = dg.textvar("x").position(dg.V2(posX[0],posX[1])).fontsize(20)
+    let y = dg.textvar("y").position(dg.V2(posY[0],posY[1])).fontsize(20)
+    let z = dg.textvar("z").position(dg.V2(posZ[0],posZ[1])).fontsize(20)
+    let tri2 = tri.combine(ang).rotate(rot)
+    if (bound) {return [bounding(bound[0],bound[1]), tri2, x, y, z]}
     return [tri2, x, y, z]
 }
+
+export function hypotenuse1() {return hypotenuse([1.3,2], [-1.2,0.6], [-0.2,0.9], [-0.5,-0.4], 2)}
+
+export function hypotenuse2() {return hypotenuse([1.5,1.5], [-0.6,-0.4], [0.6,-0.4], [0,-1.2], Math.PI*-0.75, [2.5, 2.5])}
+
+export function hypotenuse3() {return hypotenuse([1,2], [0.4,0.35], [1,-0.5], [0.3,-0.9], Math.PI*-0.35)}
